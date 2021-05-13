2f9f9479ee80b154f280b0cabc348e99ed9f7884
xiexiongxiong <xiexiongxiong@xxribd.org.cn>
2021-05-13 21:55:06 +0800

    fix

--- seata-shardingshre/shardingsphere-seata-business/src/main/java/io/seata/samples/shardingsphere/modules/mock/IOrderServiceMockImpl.java
+++ seata-shardingshre/shardingsphere-seata-business/src/main/java/io/seata/samples/shardingsphere/modules/mock/IOrderServiceMockImpl.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import io.seata.samples.shardingsphere.modules.entity.OrderEntity;
 import io.seata.samples.shardingsphere.modules.mapper.OrderMapper;
 import io.seata.samples.shardingsphere.modules.service.IOrderService;
+import org.springframework.boot.autoconfigure.session.JdbcSessionDataSourceInitializer;
 
 public class IOrderServiceMockImpl extends ServiceImpl<OrderMapper, OrderEntity> implements IOrderService {
     @Override
     public String insertOrder(OrderEntity orderEntity) {
         System.out.println("OrderService RpcException");
+        System.out.println("haha");
         return "fail";
     }
 }
